05e626655e874384104a0e7eab612027d22ed32c
fbayanatiDotData <farhad.bayanati@dotData.com>
2026-04-03 21:33:29 -0600

    tasbBoard :: add TaskPriority type

--- src/store/task.model.ts
+++ src/store/task.model.ts
@@ -1,11 +1,12 @@
 export type TaskStatus = 'todo' | 'in-progress' | 'done';
+export type TaskPriority = 'low' | 'medium' | 'high';
 
 export interface Task {
   readonly id: number;
   readonly title: string;
   readonly description: string;
   readonly status: TaskStatus;
-  readonly priority: 'low' | 'medium' | 'high';
+  readonly priority: TaskPriority;
   readonly createdAt: Date;
 }
 
